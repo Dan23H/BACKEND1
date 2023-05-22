c7516e68b4bacb3982e3935f7af310cd83ca60a5
JuanJSaavedra52 <juan_jose.saavedra@uao.edu.co>
2023-05-22 17:46:33 -0500

    Tratando de hacer el merge

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('dotenv').config();
 const cors = require('cors');
+app.use('/', require('./routes/auth'))
 
 //Crear Express App
 const app = express();
