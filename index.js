db3479b511991b0771dd83afaf630a2302333b81
HexDan <daniel.hernandez_val@uao.edu.co>
2023-05-31 00:18:05 -0500

    modified:   index.js 	deleted:    server/server.js

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const Server = require('./server/server')
+const express = require("express");
+require('dotenv').config();
+const cors = require('cors');
+const multer = require('multer');
+const sharp = require('sharp')
+const { dbConnection } = require('./database/config')
 
-const NewServer = new Server()
-NewServer.listen()
+//Crear Express App
+const app = express();
+app.use(cors());
+
+//Database
+dbConnection()
+
+//Para que escuche el body
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'))
+
+//Rutas
+app.use('/',require('./routes/auth'))
+//app.use('/images', require('./routes/image'));
+app.get('/test', (req, res) => {
+    res.json({message: "FUNCIONAAAAA"});
+})
+
+//app.use('/api/auth', require('./routes/auth'))
+
+//Escuchar en puerto 27000
+app.listen(process.env.PORT, () => {
+    Puerto = process.env.PORT
+    console.log(`Servidor corriendo en puerto: ${Puerto}`)
+})
